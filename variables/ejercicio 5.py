de2d978a72727a95815b5d3597d461581cf41f6c
hp <josue.ocampo@ucb.edu.bo>
2020-09-27 11:30:26 -0400

    Ejercicio 5

---
+++ variables/ejercicio 5.py
@@ -0,0 +1,9 @@
+Horas_trabajadas = input("Ingrese el numero de horas trabajadas:")
+print = ("var1")
+var1 = "8"
+Paga_por_hora = input("Paga por hora:")
+print = ("var1")
+var2 = "200"
+Monto_a_pagar = input("Monto a pagar:")
+print = ("var1*var2")
+
