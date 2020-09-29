a00354a7f5b1e862aa357d04ee094a6177020f85
hp <josue.ocampo@ucb.edu.bo>
2020-09-29 17:36:48 -0400

    Ejercicio # 5

--- variables/ejercicio 5.py
+++ variables/ejercicio 5.py
@@ -1,9 +1,10 @@
-Horas_trabajadas = input("Ingrese el numero de horas trabajadas:")
-print = ("var1")
-var1 = "8"
-Paga_por_hora = input("Paga por hora:")
-print = ("var1")
-var2 = "200"
-Monto_a_pagar = input("Monto a pagar:")
-print = ("var1*var2")
+horas_trabajadas = input("Ingrese el numero de horas trabajadas: ")
+paga_por_hora = input("Ingrese paga por hora: ")
+
+horas_trabajadas = int(horas_trabajadas)
+paga_por_hora = float(paga_por_hora)
+
+monto_a_pagar = horas_trabajadas * paga_por_hora
+mensaje = "Monto total a pagar: " + str(monto_a_pagar)
+print(mensaje)
 
