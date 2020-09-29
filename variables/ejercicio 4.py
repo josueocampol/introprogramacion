a6f5f9b691dedc58b1d0731c01de0f20746b31c8
hp <josue.ocampo@ucb.edu.bo>
2020-09-29 16:52:40 -0400

    Ejercicio 4

--- variables/ejercicio 4.py
+++ variables/ejercicio 4.py
@@ -1,2 +1,2 @@
-autor = input("Nombre del autor del mensaje: ")
-print("Albert Einstein dijo una vez: ¨Cada dia sabemos mas y entendemos menos¨")
+mensaje = "Albert Einstein dijo una vez: \"Cada dia sabemos mas y entendemos menos\""
+print(mensaje)
