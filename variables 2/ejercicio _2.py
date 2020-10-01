e108ea720046df0422d40fdd0e295ce3ebb86154
hp <josue.ocampo@ucb.edu.bo>
2020-10-01 19:34:25 -0400

    Agregar ejercicio 2 de variables II

---
+++ variables 2/ejercicio _2.py
@@ -0,0 +1,5 @@
+dividendo = int(input("introducir el dividendo: "))
+divisor = int(input("introducir el divisor: "))
+resto = dividendo % divisor
+respuesta = f"El resto odtenido al dividir {dividendo} entre {divisor} es {resto}"
+print(respuesta)
