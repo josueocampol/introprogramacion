85d5699ac5c37cb5e62ff0e4012ffc5f957fbfa1
hp <josue.ocampo@ucb.edu.bo>
2020-10-01 19:57:35 -0400

    Agregar ejercicio 3 de variables II

---
+++ variables 2/ejercicio_3.py
@@ -0,0 +1,5 @@
+radio = int(input("Ingrese el radio de un circulo: "))
+area = 3.1416 * radio**2
+perimetro = 2 * 3.1416 * radio
+mensaje = f"El area del circulo es igual a: {round(area, 2)}. El perimetro del circulo es igual a: {round(perimetro, 2)}"
+print(mensaje)
