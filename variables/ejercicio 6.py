a380c11dc5d5d60be9e371db1eca70087621de1c
hp <josue.ocampo@ucb.edu.bo>
2020-10-01 21:46:28 -0400

    Agregar ejercicio 6 de variables II

---
+++ variables/ejercicio 6.py
@@ -0,0 +1,6 @@
+Peso_en_kilogramos = int(input("Introduzca su peso: "))
+Estatura_en_metros = int(input("Introduzca su estatura: "))
+
+imc = Peso_en_kilogramos // Estatura_en_metros**2
+
+print(f"Tu indice de masa corporal es: {Peso_en_kilogramos//Estatura_en_metros**2}")
