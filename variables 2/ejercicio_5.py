129c25d98d497a030e066688e32e586ce99aa4f1
hp <josue.ocampo@ucb.edu.bo>
2020-10-01 21:09:05 -0400

    Agregar ejercicio 5 de variables II

---
+++ variables 2/ejercicio_5.py
@@ -0,0 +1,12 @@
+num1 = int(input("Ingrese el numero entero: "))
+El_numero_es_igual_a_5 = num1 == 5
+print(f"El numero es igual a 5: {num1==5}")
+
+El_numero_es_igual_a_50 = num1 == 50
+print(f"El numero es igual a 5.0: {num1==50}")
+
+El_numero_es_mayor_que_0_y_menor_que_10 = num1 > 0, num1 < 10
+print(f"El numero es mayor que 0 y menor que 10: {num1>0,num1<10}")
+
+El_numero_es_menor_que_0_o_mayor_que_10 = num1 < 0, num1 > 10
+print(f"El numero es menor que 0 o mayor que 10: {num1<0,num1>10}")
