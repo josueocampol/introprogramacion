1d94429b80ee99ff587275bd69f17c88fa5767dd
hp <josue.ocampo@ucb.edu.bo>
2020-10-01 20:20:09 -0400

    Agregar ejercicio 4 de variables II

---
+++ variables 2/ejercicio_4.py
@@ -0,0 +1,14 @@
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+son_iguales = num1 == num2
+print(f"los numeros son iguales: {son_iguales}")
+
+son_distintos = num1 != num2
+print(f"los numeros son diferentes: {son_distintos}")
+
+num1_mayor_que_num2 = num1>num2
+print(f"el num1 es mayor que num2: {num1_mayor_que_num2}")
+
+num1_menor_igual_a_num2 = num2<=num2
+print(f"el num1 es menor igual a num2: {num1_menor_igual_a_num2}")
+
