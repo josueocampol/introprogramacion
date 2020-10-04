dac766d1ac45c098e53aa5ed45c51e9f71bbade3
hp <josue.ocampo@ucb.edu.bo>
2020-10-04 11:50:32 -0400

    Ejercicio 3

---
+++ Flujo_condicional/Ejercicio_3.py
@@ -0,0 +1,9 @@
+Partidos_a_elegir = print("Ingrese un partido entre MAS o UNIR")
+partido1 = "MAS"
+partido2 = "MNR"
+print(partido1, partido2)
+voto = input("Seleccione un partido: ")
+if  voto == partido1 or voto == partido2 :
+    print(voto)
+else :
+    print("Voto Invalido")
