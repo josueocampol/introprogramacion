d65f1ef6a22438b9f3df4c40eded051debda9976
hp <josue.ocampo@ucb.edu.bo>
2020-10-04 10:51:45 -0400

    Ejercicio 2

---
+++ Flujo_condicional/Ejercicio_2.py
@@ -0,0 +1,8 @@
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+if num1 < num2 :
+    print(f"{num1} es menor que {num2}")
+elif num1 == num2 :
+    print(f"{num1} es igual a {num2}")
+else :
+    print(f"{num1} es mayor que {num2}")
