08c8b3d9f618a4bb3dad90198c8507bc0c682fd3
hp <josue.ocampo@ucb.edu.bo>
2020-10-08 07:34:18 -0400

    Ejercicio 1

--- flujo_ciclico/ejercicio_1.py
+++ flujo_ciclico/ejercicio_1.py
@@ -1,4 +1,4 @@
 num1 = int(input("Digite un numero positivo: "))
 while   num1 < 0:
     print("Error: El numero es negativo")
-    num = int(input("Ingrese otro numero positivo: "))
+    num1 = int(input("Ingrese otro numero positivo: "))
