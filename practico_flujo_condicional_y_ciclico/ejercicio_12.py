71ddf725b147ff6707073be0ac0099ff14e900e1
hp <josue.ocampo@ucb.edu.bo>
2020-10-13 10:32:02 -0400

    Ejercicio 12

---
+++ practico_flujo_condicional_y_ciclico/ejercicio_12.py
@@ -0,0 +1,15 @@
+num = int(input("Cantidad de palabras: "))
+palabra1 = input("palabra 1: ")
+palabra2 = input("palabra 2: ")
+len(palabra1)
+while num==(1,10):
+    if (palabra1,10):
+        print(f"palabra {1,10}")
+len(palabra1)
+len(palabra2)
+if  len(palabra1) > len(palabra2):
+    print(f"La palabra: {palabra1} tiene {len(palabra1)-len(palabra2)} letras más que {palabra2}")
+elif    len(palabra2) > len(palabra1):
+    print(f"La palabra: {palabra2} tiene {len(palabra2)-len(palabra1)} letras más que {palabra1}")
+else:
+    print("Las dos palabras tienen el mismo largo")
