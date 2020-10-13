45ffa3d6970050af43ceecf17fcff758455fdc74
hp <josue.ocampo@ucb.edu.bo>
2020-10-13 10:09:07 -0400

    Ejercicio 11

---
+++ practico_flujo_condicional_y_ciclico/ejercicio_11.py
@@ -0,0 +1,14 @@
+print("Ingrese su fecha de nacimiento.")
+dia = int(input("Día: "))
+mes = int(input("Mes: "))
+año = int(input("Año: "))
+from time import localtime
+t = localtime()
+t.tm_mday
+11
+t.tm_mon
+10
+t.tm_year
+2020
+if año<2020:
+    print(f"Usted tiene {-año+2020} años")
