07b12aeffe81487fe0119d6938bd59a2b35262f6
hp <josue.ocampo@ucb.edu.bo>
2020-10-14 11:02:30 -0400

    Ejercicio 13

--- practico_flujo_condicional_y_ciclico/ejercicio_13.py
+++ practico_flujo_condicional_y_ciclico/ejercicio_13.py
@@ -1,8 +1,11 @@
-def NumeroN(n):
-        numero = ((-1)**n)/((2*n)+1)
-        return numero
-
-pi = 0
-for i in range(10000):
-    pi = pi + NumeroN(i)
-print(pi*4)
+numero_de_terminos = int(input("n: "))
+suma = 0
+for i in range(1, numero_de_terminos):
+    if i % 2 == 0:
+        signo =-1
+    else:
+        signo = 1
+    valor_termino_actual = signo / (1 + 2*(i-1))
+    suma += valor_termino_actual
+pi = 4*suma
+print(pi)
