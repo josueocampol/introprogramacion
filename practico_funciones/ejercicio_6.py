96340eb634db53ed6f592b8a4852c4eb2807929b
hp <josue.ocampo@ucb.edu.bo>
2020-10-27 17:02:19 -0400

    ejercicio_6

--- practico_funciones/ejercicio_6.py
+++ practico_funciones/ejercicio_6.py
@@ -1,23 +1,14 @@
-def inversa(cadena):
-    invertida=""
-    cont=len(cadena)
-    indice=-1
-    while cont>=1:
-        invertida += cadena[indice]
-        indice=indice+(-1)
-        cont=1
-    return invertida
-def es_palindromo (cadena):
-    palabra_invertida = inversa(cadena)
-    indice=0
-    cont=0
-    for i in range(len(cadena)):
-        if palabra_invertida[indice]==cadena[indice]:
-            indice+=1
-            cont+=1
-        else:
-            print("No es palindromo")
-            break
+def palindromo(palabra):
+    x=len(palabra)
+    x = x-1
+    palabra_invertida = ""
+    for i in range(x, -1, -1):
+        palabra_invertida = palabra_invertida + palabra[i]
+    palabra2 = palabra_invertida
+    if palabra == palabra2 :
+        print("True.")
+    else:
+        print("False.")
 
-    if cont==len(cadena):
-        print("Es palindromo")
+palabra = input("Ingrese palabra: ")
+palindromo(palabra)
