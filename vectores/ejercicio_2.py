ba9cfe3d3a42b11b0c6c344685a58d70856b076f
hp <josue.ocampo@ucb.edu.bo>
2020-11-22 18:36:15 -0400

    ejercicio_2

---
+++ vectores/ejercicio_2.py
@@ -0,0 +1,17 @@
+a1=int(input("Ingrese 1er número de la lista 1: "))
+a2=int(input("Ingrese 2do número de la lista 1: "))
+a3=int(input("Ingrese 3er número de la lista 1: "))
+a4=int(input("Ingrese 4to número de la lista 1: "))
+a5=int(input("Ingrese 5to número de la lista 1: "))
+V1=[a1,a2,a3,a4,a5]
+print(V1)
+b1=int(input("Ingrese 1er número de la lista 2: "))
+b2=int(input("Ingrese 2do número de la lista 2: "))
+b3=int(input("Ingrese 3er número de la lista 2: "))
+b4=int(input("Ingrese 4to número de la lista 2: "))
+b5=int(input("Ingrese 5to número de la lista 2: "))
+V2=[b1,b2,b3,b4,b5]
+print(V2)
+vectorR=[a1+b1,a2+b2,a3+b3,a4+b4,a5+b5]
+print("El vector suma es: ")
+print(vectorR)
